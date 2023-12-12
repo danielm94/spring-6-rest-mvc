ad3cf89b994d193772c257f92aee667b7f35cfb3
Daniel Martins <danielm94@users.noreply.github.com>
2023-12-12 16:49:33 -0500

    Add abstract method for listing beers to beer service interface

--- src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
@@ -2,8 +2,11 @@ package guru.springframework.spring6restmvc.services;
 
 import guru.springframework.spring6restmvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
     Beer getBeerById(UUID id);
+
+    List<Beer> listBeers();
 }
